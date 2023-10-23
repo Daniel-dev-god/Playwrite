e6efd85397375efdd88ba7b8a938ff81da2dcdf0
Daniel-dev-god <danredrock177@gmail.com>
2023-10-23 23:35:54 +0200

    update

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-from playwright.sync_api import Page
-from playwright.sync_api import sync_playwright
-
-
-class Login:
-
-    def __init__(self, page):
-        self.page = page
-
-    def sign_in(self):
-        self.page.goto("https://www.saucedemo.com/")
-        self.page.locator("[data-test=\"username\"]").click()
-        self.page.locator("[data-test=\"username\"]").fill("standard_user")
-        self.page.locator("[data-test=\"password\"]").click()
-        self.page.locator("[data-test=\"password\"]").fill("secret_sauce")
-        self.page.locator("[data-test=\"login-button\"]").click()
+# from playwright.sync_api import Page
+# from playwright.sync_api import sync_playwright
 
 
 def test_checkout_items(page):
@@ -42,80 +28,63 @@ def test_checkout_items(page):
 class ShoppingCartTest:
     def __init__(self, page):
         self.page = page
-
-    def login(self, username: str, password: str):
-        self.page.goto(url="https://www.saucedemo.com/")
-        self.page.locator("[data-test=\"username\"]").click()
-        self.page.locator("[data-test=\"username\"]").fill(username)
-        self.page.locator("[data-test=\"password\"]").click()
-        self.page.locator("[data-test=\"password\"]").fill(password)
-        self.page.locator("[data-test=\"login-button\"]").click()
+        self.backpack_add = self.page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]")
+        self.bike_light_add = self.page.locator("[data-test=\"add-to-cart-sauce-labs-bike-light\"]")
+        self.fleece_jacket_add = self.page.locator("[data-test=\"add-to-cart-sauce-labs-fleece-jacket\"]")
+        self.bolt_tshirt_add = self.page.locator("[data-test=\"add-to-cart-sauce-labs-bolt-t-shirt\"]")
+        self.onesie_add = self.page.locator("[data-test=\"add-to-cart-sauce-labs-onesie\"]")
+        self.red_tshirt_add = self.page.locator(
+            "[data-test=\"add-to-cart-test\\.allthethings\\(\\)-t-shirt-\\(red\\)\"]")
+        self.backpack_remove = self.page.locator("[data-test=\"remove-sauce-labs-backpack\"]")
+        self.bike_light_remove = self.page.locator("[data-test=\"remove-sauce-labs-bike-light\"]")
+        self.fleece_jacket_remove = self.page.locator("[data-test=\"remove-sauce-labs-fleece-jacket\"]")
+        self.bolt_tshirt_remove = self.page.locator("[data-test=\"remove-sauce-labs-bolt-t-shirt\"]")
+        self.onesie_remove = self.page.locator("[data-test=\"remove-sauce-labs-onesie\"]")
+        self.red_tshirt_remove = self.page.locator("[data-test=\"remove-test\\.allthethings\\(\\)-t-shirt-\\(red\\)\"]")
+        self.shopping_cart = self.page.locator("a").filter(has_text="6")
+        self.checkout_button = self.page.locator("[data-test=\"checkout\"]")
+        self.shipping_name = self.page.locator("[data-test=\"firstName\"]")
+        self.shipping_lastname = self.page.locator("[data-test=\"lastName\"]")
+        self.shipping_postcode = self.page.locator("[data-test=\"postalCode\"]")
+        self.confirm_order = self.page.locator("[data-test=\"finish\"]")
+        self.cancel_order = self.page.locator("[data-test=\"cancel\"]")
+        self.back_home = self.page.locator("[data-test=\"back-to-products\"]")
 
     def add_all_items_to_cart(self):
         # adds ALL items available in shop to cart
-        items_to_add = [
-            "[data-test=\"add-to-cart-sauce-labs-backpack\"]",
-            "[data-test=\"add-to-cart-sauce-labs-bike-light\"]",
-            "[data-test=\"add-to-cart-sauce-labs-fleece-jacket\"]",
-            "[data-test=\"add-to-cart-sauce-labs-bolt-t-shirt\"]",
-            "[data-test=\"add-to-cart-sauce-labs-onesie\"]",
-            "[data-test=\"add-to-cart-test\\.allthethings\\(\\)-t-shirt-\\(red\\)\"]",
-        ]
-
-        for item in items_to_add:
-            self.page.locator(item).click()
+        self.backpack_add().click()
+        self.bike_light_add.click()
+        self.fleece_jacket_add.click()
+        self.bolt_tshirt_add.click()
+        self.onesie_add.click()
+        self.red_tshirt_add.click()
 
     def remove_all_items_to_cart(self):
         # removes ALL items available in shop to cart
-        items_to_add = [
-            "[data-test=\"remove-sauce-labs-backpack\"]",
-            "[data-test=\"remove-sauce-labs-bike-light\"]",
-            "[data-test=\"remove-sauce-labs-bolt-t-shirt\"]",
-            "[data-test=\"remove-sauce-labs-fleece-jacket\"]",
-            "[data-test=\"remove-sauce-labs-onesie\"]",
-            "[data-test=\"remove-test\\.allthethings\\(\\)-t-shirt-\\(red\\)\"]",
-        ]
-
-        for item in items_to_add:
-            self.page.locator(item).click()
+        self.backpack_remove().click()
+        self.bike_light_remove.click()
+        self.fleece_jacket_remove.click()
+        self.bolt_tshirt_remove.click()
+        self.onesie_remove.click()
+        self.red_tshirt_remove.click()
 
     def checkout(self):
-        self.page.locator("a").filter(has_text="6").click()
-        self.page.locator("[data-test=\"checkout\"]").click()
+        self.shopping_cart().click()
+        self.checkout_button().click()
 
-    def fill_shipping_info(self, firstname, lastname, postalcode: int):
-        self.page.locator("[data-test=\"firstName\"]").click()
-        self.page.locator("[data-test=\"firstName\"]").fill(firstname)
-        self.page.locator("[data-test=\"firstName\"]").press("Tab")
-        self.page.locator("[data-test=\"lastName\"]").fill(lastname)
-        self.page.locator("[data-test=\"lastName\"]").press("Tab")
-        self.page.locator("[data-test=\"postalCode\"]").fill(postalcode)
+    def fill_shipping_info(self, firstname: str, lastname: str, postalcode: int):
+        self.shipping_name.fill(firstname)
+        self.shipping_lastname.fill(lastname)
+        self.shipping_postcode.fill(postalcode)
 
     def finalize_order(self, complete_order: bool):
         if complete_order:
-            self.page.locator("[data-test=\"finish\"]").click()
+            self.confirm_order.click()
         else:
-            self.page.locator("[data-test=\"cancel\"]").click()
+            self.cancel_order.click()
 
     def navigate_back_to_products(self):
-        self.page.locator("[data-test=\"back-to-products\"]").click()
-
-    def test_run_test(self):
-        self.login("standard_user", "secret_sauce")
-        self.add_all_items_to_cart()
-        self.remove_all_items_to_cart()
-        self.add_all_items_to_cart()
-        self.checkout()
-        self.fill_shipping_info("Steven", "Smith", 77653)
-        self.finalize_order(True)
-        self.navigate_back_to_products()
+        self.back_home.click()
 
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
 
-    test = ShoppingCartTest(page)
-    test.test_run_test()
-
-    browser.close()
